413b549c7b9cd6e4d402545558263573a69d04ba
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-05-03 03:12:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace sayiTahminOyunu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rand = new Random();
+            int sayi = rand.Next(1, 100);
+            int tahmin, sayac = 0;
+            do
+            {
+                sayac = sayac + 1;
+                Console.Write(sayac + ". Tahmin: ");
+                tahmin = int.Parse(Console.ReadLine());
+                if (tahmin < sayi && sayac < 7)
+                {
+                    Console.WriteLine("Daha büyük bir sayı girin");
+                }
+                else if (tahmin > sayi && sayac < 7)
+                {
+                    Console.WriteLine("Daha küçük bir sayı girin");
+                }
+
+            } while (tahmin != sayi && sayac != 7);
+            if (tahmin == sayi)
+            {
+                Console.WriteLine("Tebrikler " + sayac + " denemede bildiniz");
+            }
+            else
+            {
+                Console.WriteLine("Oyun bitti, bilemediniz. Sayı : " + sayi);
+            }
+            Console.Write("Uygulamayı kapatmak için bir tuşa basınız.");
+            Console.ReadKey();
+        }
+    }
+}
